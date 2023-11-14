bd6b15b6140562a63f2c2c0fd9c995b886cef331
Javier Moneo <jvr.moneo@gmail.com>
2023-11-14 20:23:02 +0100

    Finalizando la funcionalidad de LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,17 @@ function App() {
   const [paciente, setPaciente] = useState({});
 
   useEffect(() => {
-    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+  }, []);
+
+  useEffect(() => {
+    if (pacientes) {
+      localStorage.setItem("pacientes", JSON.stringify(pacientes));
+    }
   }, [pacientes]);
 
   const eliminarPaciente = (id) => {
