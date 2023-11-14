cfd480c27e550a64c39d87d0698b3494bd85be54
Javier Moneo <jvr.moneo@gmail.com>
2023-11-14 11:24:33 +0100

    Aplicando clases al Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+        Seguimiento pacientes{" "}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
+    </>
   );
 };
