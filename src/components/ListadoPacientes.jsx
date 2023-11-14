19139a9a8fadb7175b5cbe93f0b53f5599f2a09b
Javier Moneo <jvr.moneo@gmail.com>
2023-11-14 18:03:57 +0100

    Iterando sobre un array en React

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,6 @@
 import { Paciente } from "./Paciente";
 
-export const ListadoPacientes = () => {
+export const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen md:overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
@@ -10,13 +10,9 @@ export const ListadoPacientes = () => {
           Pacientes y Citas
         </span>
       </p>
-
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
-      <Paciente />
+      {pacientes.map((paciente) => (
+        <Paciente paciente={paciente} />
+      ))}
     </div>
   );
 };
