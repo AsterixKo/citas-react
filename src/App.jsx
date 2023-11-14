1b0281e6e54e9a6d7cf5a868c6fc0e57e001492b
Javier Moneo <jvr.moneo@gmail.com>
2023-11-14 10:00:10 +0100

    Crear componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Formulario } from "./components/Formulario";
+import { Header } from "./components/Header";
+import { ListadoPacientes } from "./components/ListadoPacientes";
+
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>Hola mundo</h1>
-      </div>
+      <Header />
+      <Formulario />
+      <ListadoPacientes/>
     </>
   );
 }
