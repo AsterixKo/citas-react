8fce4ab9c826bcb97bbdec31be085b6e2bbe4370
Javier Moneo <jvr.moneo@gmail.com>
2023-11-14 19:04:28 +0100

    Colocando en el State el paciente a editar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,6 @@
 export const Paciente = ({ paciente, setPaciente }) => {
   const { nombre, propietario, email, fecha, sintomas } = paciente;
+
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
